d92343113b8e4df4360c0db907d089d001614dc1
Sumtudou <1163015195@qq.com>
2020-07-23 20:07:35 +0800

    [guess num][Jay] refactor:Extract the sumA and sumAB methods from GuessNumber.guess

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -7,10 +7,32 @@ public class GuessNumber {
         this.answer = answer;
     }
 
-    public String guess(int inputNumArr[]) {
-        int sumA = 0;
-        int sumAB = 0;
+    public String guess(int[] inputNumArr) {
+        int sumA = this.getSumA(inputNumArr);   //sum of A
+        int sumAB = this.getSumAB(inputNumArr); // sum of (A add B)
 
+        if (sumA == 4) {
+            return "4A0B";
+        }
+
+        if (sumAB == 0) {
+            return "0A0B";
+        }
+        return null;
+    }
+
+    public int getSumA(int[] inputNumArr){
+        int sumA = 0;
+        for (int i = 0; i < inputNumArr.length; i++) {
+            if (inputNumArr[i] == answer[i]) {
+                sumA++;
+            }
+        }
+        return sumA;
+    }
+
+    public  int getSumAB(int[] inputNumArr){
+        int sumAB = 0;
         for (int inputNumItem : inputNumArr) {
             for (int answerItem : answer) {
                 if (answerItem == inputNumItem) {
@@ -19,19 +41,6 @@ public class GuessNumber {
                 }
             }
         }
-
-        for (int i = 0; i < inputNumArr.length; i++) {
-            if (inputNumArr[i] == answer[i]) {
-                sumA++;
-            }
-        }
-        if (sumA == 4) {
-            return "4A0B";
-        }
-
-        if(sumAB == 0){
-            return "0A0B";
-        }
-        return null;
+        return sumAB;
     }
 }
