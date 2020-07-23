21a5ac40f83b16837ba18f9ec98fb27d08b1e44b
Sumtudou <1163015195@qq.com>
2020-07-23 19:43:53 +0800

    [guess num][Jay] feature:pass should_return_4A0B_when_guess_given_all_correct

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -6,6 +6,15 @@ public class GuessNumber {
         this.answer = answer;
     }
     public String guess(int inputNumArr[]) {
+        int sumA = 0;
+        for (int i = 0; i < inputNumArr.length; i++) {
+            if (inputNumArr[i] == answer[i]) {
+                sumA++;
+            }
+        }
+        if (sumA == 4) {
+            return "4A0B";
+        }
         return null;
     }
 }
