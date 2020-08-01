0968727bd13a5248bf368c3f81ebb9ef5652fbb8
Sumtudou <1163015195@qq.com>
2020-08-01 16:49:58 +0800

    [guess num][Jay] refactor:exact some code to setUp method in GuessNumberTest

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -1,5 +1,6 @@
 package example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -8,15 +9,19 @@ import static org.mockito.Mockito.when;
 
 public class GuessNumberTest {
 
-    public int[] getAnswer(){
-        GenerateNumber generateNumbers = Mockito.mock(GenerateNumberImpl.class);
-        when(generateNumbers.getAnswer()).thenReturn(new int[]{1,2,3,4});
-        return  generateNumbers.getAnswer();
+
+    GenerateNumber generateNumbers;
+    @BeforeEach
+    void setUp() {
+        generateNumbers = Mockito.mock(GenerateNumberImpl.class);
+        when(generateNumbers.getAnswer()).thenReturn(new int[]{1, 2, 3, 4});
     }
+
     @Test
-    public void should_return_4A0B_when_guess_given_1234() {
+
+    public void should_return_4A0B_when_guess_given_input_number_1234() {
         //given
-        int[] answer = getAnswer();
+        int[] answer = generateNumbers.getAnswer();
         int[] inputNumArr = {1, 2, 3, 4};
         GuessNumber guessNumber = new GuessNumber(answer);
         //when
@@ -26,9 +31,9 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_return_0A0B_when_guess_given_5678() {
+    public void should_return_0A0B_when_guess_given_input_number_5678() {
         //given
-        int[] answer = getAnswer();
+        int[] answer = generateNumbers.getAnswer();
         int[] inputNumArr = {5, 6, 7, 8};
         GuessNumber guessNumber = new GuessNumber(answer);
         //when
@@ -39,9 +44,9 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_return_2A0B_when_guess_given_1278() {
+    public void should_return_2A0B_when_guess_given_input_number_1278() {
         //given
-        int[] answer = getAnswer();
+        int[] answer = generateNumbers.getAnswer();
         int[] inputNumArr = {1, 2, 7, 8};
         GuessNumber guessNumber = new GuessNumber(answer);
         //when
@@ -51,9 +56,9 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_return_0A2B_when_guess_given_3478() {
+    public void should_return_0A2B_when_guess_given_input_number_3478() {
         //given
-        int[] answer = getAnswer();
+        int[] answer = generateNumbers.getAnswer();
         int[] inputNumArr = {3, 4, 7, 8};
         GuessNumber guessNumber = new GuessNumber(answer);
         //when
@@ -63,9 +68,9 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_return_2A2B_when_guess_given_1243() {
+    public void should_return_2A2B_when_guess_given_input_number_1243() {
         //given
-        int[] answer = getAnswer();
+        int[] answer = generateNumbers.getAnswer();
         int[] inputNumArr = {1, 2, 4, 3};
         GuessNumber guessNumber = new GuessNumber(answer);
         //when
@@ -75,9 +80,9 @@ public class GuessNumberTest {
     }
 
     @Test
-    public void should_return_1A1B_when_guess_given_1829() {
+    public void should_return_1A1B_when_guess_given_input_number_1829() {
         //given
-        int[] answer = getAnswer();
+        int[] answer = generateNumbers.getAnswer();
         int[] inputNumArr = {1, 8, 2, 9};
         GuessNumber guessNumber = new GuessNumber(answer);
         //when
